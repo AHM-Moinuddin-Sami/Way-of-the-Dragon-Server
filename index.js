9bb7004b6a3b2d8bb59af3be9e1ea58970c6b0b7
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-06-08 20:40:24 +0600

    added jwt with functions and attached users

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const jwt = require('jsonwebtoken');
 app.use(express.json());
 app.use(cors());
 
+// JWT verification
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 // MongoDB initialization part
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -24,11 +43,54 @@ const client = new MongoClient(uri, {
     }
 });
 
+const database = client.db("WayOfTheDragonDB");
+
 //   All APIs
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
+
+        const usersCollection = database.collection("users");
+        const classesCollection = database.collection("classes");
+
+        // Admin verification
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
+        // user APIs
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email };
+
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'This user already exists in the database.' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+
+            res.send(result);
+        })
+
+        // classes APIs
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
